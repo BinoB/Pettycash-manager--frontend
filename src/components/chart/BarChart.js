9da4b73a29c3b9e9492a66639d8ccee4c265d6ee
BinoB <binob831@gmail.com>
2023-08-10 17:31:04 +0530

    latest modified

--- src/components/chart/BarChart.js
+++ src/components/chart/BarChart.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Bar} from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import './Chart.scss';
-
+//
 
 function BarChart({ chartData }) {
   return (<div className="chart-container">
